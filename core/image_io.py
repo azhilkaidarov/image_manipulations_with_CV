22572bf5b4ddfc77c36251044ecaf24cb885d6a8
Asylbek Zhilkaidarov <asylzhilkaidarov@icloud.com>
2026-02-02 16:21:10 -0600

    Add Pillow dependency and implement image loading functionality
    - Added Pillow as a dependency in pyproject.toml and uv.lock.
    - Implemented image loading and error handling in core/image_io.py using the Pillow library.
    - Defined an Enum for image sources to streamline image management.

--- core/image_io.py
+++ core/image_io.py
@@ -1,1 +1,23 @@
 # загрузка / сохранение (PIL, matplotlib)
+import os
+from PIL import Image
+from enum import Enum
+
+SOURCE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "raw") + "/"
+
+
+class Picture(Enum):
+    BOATS = "the_boats.jpg"
+    DOOR = "the_door.jpg"
+    ISLAND = "the_island.jpg"
+    MOUNTAINS = "the_mountains.jpg"
+    TREES = "the_trees.png"
+
+
+try:
+    img = Image.open(f"{SOURCE_PATH}{Picture.ISLAND.value}")
+    img.show()
+except FileNotFoundError:
+    print("Error: The image file was not found. Please check the file path.")
+except IOError:
+    print("Error: Cannot open the image file.")
