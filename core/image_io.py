facf1861bb9be425ebdc55a1e0bc672c7f1f74fa
Asylbek Zhilkaidarov <asylzhilkaidarov@icloud.com>
2026-02-02 22:12:58 -0600

    Day 1 - Getting into image processing - filled up logbook.txt

--- core/image_io.py
+++ core/image_io.py
@@ -1,16 +1,6 @@
 # загрузка / сохранение (PIL, matplotlib)
-import os
+from pathlib import Path
 
-from PIL import Image
 from core.utils import Picture
 
-SOURCE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "raw") + "/"
-print(type(Picture.BOATS))
-
-def get_image(image: Picture):
-    try:
-        return Image.open(f"{SOURCE_PATH}{image.value}")
-    except FileNotFoundError:
-        print("Error: The image file was not found. Please check the file path.")
-    except IOError:
-        print("Error: Cannot open the image file.")
+SOURCE_PATH = Path.cwd() / 'data' / 'raw'
