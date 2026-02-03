a5af8be86e76659c66879df29f310491cd78f479
Asylbek Zhilkaidarov <asylzhilkaidarov@icloud.com>
2026-02-02 22:02:48 -0600

    День 1. - Импортировал фотки через mpimg. - Указал output. - Играюсь в playground.ipynb. - учусь работать с plt.*

--- core/image_io.py
+++ core/image_io.py
@@ -2,15 +2,15 @@
 import os
 
 from PIL import Image
-from utils import Picture
+from core.utils import Picture
 
 SOURCE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "raw") + "/"
+print(type(Picture.BOATS))
 
-
-try:
-    img = Image.open(f"{SOURCE_PATH}{Picture.MOUNTAINS.value}")
-    img.show()
-except FileNotFoundError:
-    print("Error: The image file was not found. Please check the file path.")
-except IOError:
-    print("Error: Cannot open the image file.")
+def get_image(image: Picture):
+    try:
+        return Image.open(f"{SOURCE_PATH}{image.value}")
+    except FileNotFoundError:
+        print("Error: The image file was not found. Please check the file path.")
+    except IOError:
+        print("Error: Cannot open the image file.")
